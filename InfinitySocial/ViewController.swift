adcae4ea0f3dad6521525e57c76d52f196b7f3a3
Qaiser Abbas <qaiser.abbas.uett@gmail.com>
2019-01-08 09:59:07 +0500

    Initial commit

---
+++ InfinitySocial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InfinitySocial
+//
+//  Created by Qaiser on 1/6/19.
+//  Copyright © 2019 Infinity Creatives. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
